0cffee8ebe37232fdb2b02d2d7a6ea62aafbf135
GitIPS <torsten.beck@@onlinehome.de>
2020-08-31 16:31:34 +0200

    ww

--- MyRaspStat/module.php
+++ MyRaspStat/module.php
@@ -54,6 +54,7 @@ class MyRaspberryPi extends IPSModule
         $this->RegisterPropertyBoolean("IPS_Server", false);
         
         
+
         //Float Variable anlegen
         $variablenID =  $this->RegisterVariableInteger("ID_cpuFreq", "CPU frequency","Hertz", 1);
         IPS_SetInfo ($variablenID, "WSS"); 
