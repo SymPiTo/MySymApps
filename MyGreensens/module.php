d7696a4c3d45be848fd16d07cca04400db89dd2f
GitIPS <torsten.beck@@onlinehome.de>
2021-04-25 19:21:26 +0200

    Greensens

--- MyGreensens/module.php
+++ MyGreensens/module.php
@@ -327,7 +327,7 @@ ________________________________________________________________________________
             $data = json_decode($resp, true);
             $plantdata = $data['data']['registeredHubs'][0]['plants'];
             //Daten in Variablen schreiben
-            $totalSensors = count($plantdat);
+            $totalSensors = count($plantdata);
             for ($zaehler = 0; $zaehler <= $totalSensors-1; $zaehler++) {
                 
                 $this->SetValue("sensorID".$zaehler, $plantdata[$zaehler]['sensorID']);
